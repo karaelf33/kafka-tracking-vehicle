b41c615f34075c4814a6f40d28f79f6ce0798da6
yusuf <bestasyusuf8@gmail.com>
2022-11-18 19:45:20 +0100

    kafka producer +consumer

---
+++ src/test/java/com/example/kafkatrackingvehicle/KafkaTrackingVehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkatrackingvehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTrackingVehicleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
